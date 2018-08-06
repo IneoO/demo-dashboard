3d38a8400b0be1ed81b868917809bd14bb3cf7c9
Valère Pique <pique.valere@gmx.fr>
2018-08-06 21:54:45 +0200

    Refacto menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <Header @showMenu="showMenu"/>
+    <Menu />
     <div class="full-height">
-      <Menu />
       <div class="container">
         <router-view />
       </div>
@@ -30,6 +30,47 @@ export default {
 
 <style lang="scss">
   @import './assets/scss/main';
+
+  @media screen and (max-width: 1087px) {
+    .container {
+      margin: 10px;
+      max-width: inherit;
+      width: auto;
+    }
+  }
+
+  @media screen and (min-width: 1088px) {
+    .container {
+      margin-left: 240px;
+      margin-top: 30px;
+      margin-right: 40px;
+      margin-bottom: 40px;
+      max-width: inherit;
+      width: auto;
+    }
+  }
+
+  @media screen and (min-width: 1280px) {
+    .container {
+      margin-left: 240px;
+      margin-top: 30px;
+      margin-right: 40px;
+      margin-bottom: 40px;
+      max-width: inherit;
+      width: auto;
+    }
+  }
+
+  @media screen and (min-width: 1472px) {
+    .container {
+      margin-left: 240px;
+      margin-top: 30px;
+      margin-right: 40px;
+      margin-bottom: 40px;
+      max-width: inherit;
+      width: auto;
+    }
+  }
 
   html, body, #app{
     overflow-y: hidden;
--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -22,7 +22,7 @@
       <div class="modal-card">
         <aside class="menu mobile-display" v-clickOutside="external">
           <a class="navbar-item header-brand" href="">
-            <img src="../../assets/img/getTech.png" alt="GetTech" width="112" height="56">
+            <img src="../../assets/img/getTech_w.png" alt="GetTech" width="112" height="56">
           </a>
           <div v-for="menu in menus"
             :key="menu.id">
@@ -32,7 +32,7 @@
             <ul class="menu-list">
               <li v-for="submenu in menu.submenus"
                 :key="submenu.id">
-                <router-link v-link="submenu.href">
+                <router-link :to="submenu.href" active-class="is-active" exact>
                   <i :class="[...cssClass, submenu.icon]"></i>
                   <span>{{submenu.text}}</span>
                 </router-link>
